1ec7608ad388be170bd491df10ad35ceebb073a0
Charly Garibay <jcgaribayr@gmail.com>
2016-09-28 17:10:32 -0700

    Chart

--- Chart/Chart/Doughnut/DoughnutViewModel.cs
+++ Chart/Chart/Doughnut/DoughnutViewModel.cs
@@ -1,49 +1,61 @@
-﻿using System;
+﻿using Chart.Models;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Chart.Models;
-using Xamarin.Forms;
-using System.Windows.Input;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace Chart.Doughnut
 {
     public class DoughnutViewModel : INotifyPropertyChanged
     {
-        private ICommand onSelectionChangedCommand;
+        #region Chart
+        private int Chart_selectionIndex;
+        public int Chart_SelectionIndex
+        {
+            get
+            {
+                return Chart_selectionIndex;
+            }
+            set
+            {
+                Chart_selectionIndex = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
 
+        #region List
+        private Account List_selectedItem;
+        public Account List_SelectedItem
+        {
+            get
+            {
+                return List_selectedItem;
+            }
+            set
+            {
+                List_selectedItem = value;
+
+                var index = Accounts.IndexOf(value);
+                Chart_SelectionIndex = index;
+
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Property Changed
         public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string property = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        #endregion
 
         public ObservableCollection<Account> Accounts { get; set; }
 
         public List<Color> Colors { get; set; }
 
-        public string AccountName { get; set; }
-
-        public ICommand OnSelectionChangedCommand
-        {
-            get
-            {
-                return onSelectionChangedCommand;
-            }
-
-            set
-            {
-                onSelectionChangedCommand = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public void OnPropertyChanged([CallerMemberName] string property = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
-
         public DoughnutViewModel()
         {
-            OnSelectionChangedCommand = new Command<Account>(OnSelectionChanged);
-
             Accounts = new ObservableCollection<Account>();
             Accounts.Add(new Account { Description = "Checking 1", Amount = 1000 });
             Accounts.Add(new Account { Description = "Checking 2", Amount = 2000 });
@@ -54,11 +66,6 @@ namespace Chart.Doughnut
 
             Colors = new List<Color>();
             Colors.Add(Color.FromHex("#D2D4D7"));
-        }
-
-        private void OnSelectionChanged(Account account)
-        {
-            AccountName = account.Description;
         }
     }
 }
